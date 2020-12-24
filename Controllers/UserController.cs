d5d6cd8fe3286ec6cff24fa14aec5f6ac74756a8
ouyangxiao <oyangxiao@outlook.com>
2020-12-24 13:11:52 +0800

    baocun

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,39 +1,100 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using questionnaire_api.Models;
 
 namespace questionnaire_api.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly UserContext _context;
 
-        private readonly ILogger<UserController> _logger;
-
-        public UserController(ILogger<UserController> logger)
+        public UserController(UserContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        [HttpGet]
-        public IEnumerable<UserItem> Get()
+        // GET: api/User/me
+        [HttpGet("me")]
+        public async Task<ActionResult<UserItem>> GetUserItem(long id)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new UserItem
+            var userItem = await _context.UserItems.FindAsync(id);
+
+            if (userItem == null)
             {
-                Id = index,
-                Name = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound();
+            }
+
+            return userItem;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserItem(long id, UserItem userItem)
+        {
+            if (id != userItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserItem>> PostUserItem(UserItem userItem)
+        {
+            _context.UserItems.Add(userItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserItem", new { id = userItem.Id }, userItem);
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserItem(long id)
+        {
+            var userItem = await _context.UserItems.FindAsync(id);
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserItems.Remove(userItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserItemExists(long id)
+        {
+            return _context.UserItems.Any(e => e.Id == id);
         }
     }
 }
